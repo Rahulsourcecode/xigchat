6de14ba31fada8f0b7c260a9aa6d65609b86074a
Rahul R <rajeshrahul831@gmail.com>
2023-11-20 13:30:36 +0530

    established db connection

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import Express from "express"
+import { configDotenv } from "dotenv"
+import { connection } from "./configs/dbConfig.js"
+configDotenv()
+
+const PORT = process.env.PORT
+const app = Express()
+
+
+
+
+
+
+connection()
+    .then(() => console.log(`database connected`))
+    .catch((err) => console.log(`coudnt connect to db error => ${err}`))
+app.listen(PORT, async () => console.log(`server running on port ${PORT}`)) 
